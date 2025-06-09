c9f57cba990565d02ae6b179e589730e8ae239b7
Bruce ZhongZp <xzhong86@163.com>
2025-06-09 17:14:24 +0800

    little update

--- src/bus/chi/Module.hh
+++ src/bus/chi/Module.hh
@@ -1,8 +1,13 @@
 
+#include <csp/sim/Module.hh>
+#include <bus/chi/Port.hh>
+
 namespace bus::chi {
 
 class Module : public csp::Module {
-    ;
+
+    CHIPort* getPortById(int id) const;
+    CHIPort* getPortByName(string &name) const;
 };
 
 }
